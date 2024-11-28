9fe8c2f2a4539c2e279fb2b01e90f8d2318017de
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:42:21 +0530

    Update dict_q03.py

--- coding-assignment/dict/dict_q03.py
+++ coding-assignment/dict/dict_q03.py
@@ -1,1 +1,7 @@
+#Given a sentence, create a dictionary that counts how many times each word appears.
 
+sentence = "hello world hello"
+word_count = {}
+for word in sentence.split():
+    word_count[word] = word_count.get(word, 0) + 1
+print(word_count)
