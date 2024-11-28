b251a44c0bc2186eed372439d2d1d4fec2c5ebf2
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:44:40 +0530

    Update dict_q06.py

--- coding-assignment/dict/dict_q06.py
+++ coding-assignment/dict/dict_q06.py
@@ -1,1 +1,7 @@
+#Given a list with duplicates, use a dictionary to remove duplicates while maintaining order.
 
+items = ['apple', 'banana', 'apple', 'orange']
+unique_items = {}
+for item in items:
+    unique_items[item] = None
+print(list(unique_items.keys()))
