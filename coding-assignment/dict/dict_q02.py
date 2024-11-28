f6c003f65f9f80a98c8beb0c09f4a783b5a0fc29
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:41:41 +0530

    Update dict_q02.py

--- coding-assignment/dict/dict_q02.py
+++ coding-assignment/dict/dict_q02.py
@@ -1,1 +1,5 @@
+#You maintain an inventory of items with their quantities. Write a program to update the quantity of an item when new stock arrives.
 
+inventory = {'apples': 10, 'bananas': 5}
+inventory['apples'] += 20
+print(inventory)
