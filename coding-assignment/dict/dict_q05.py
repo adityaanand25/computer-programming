45d39560fed69b8e3851db57f564adb2344ef2ba
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:44:01 +0530

    Update dict_q05.py

--- coding-assignment/dict/dict_q05.py
+++ coding-assignment/dict/dict_q05.py
@@ -1,1 +1,6 @@
+#Write a program to find the key associated with the minimum value in a dictionary.
+python
 
+scores = {'Alice': 85, 'Bob': 78, 'Charlie': 90}
+min_score_key = min(scores, key=scores.get)
+print(min_score_key)
