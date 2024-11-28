855038a0de4d3f045103567628de1306f9d63963
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:46:09 +0530

    Update dict_q08.py

--- coding-assignment/dict/dict_q08.py
+++ coding-assignment/dict/dict_q08.py
@@ -1,1 +1,6 @@
+#you have two dictionaries with overlapping keys. Write a program to combine them such that values from the second dictionary overwrite those from the first.
 
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+combined_dict = {**dict1, **dict2}
+print(combined_dict)
