17c79e0ef470b4758e492d843b9c6afaa2f077bf
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:40:44 +0530

    Update dict_q01.py

--- coding-assignment/dict/dict_q01.py
+++ coding-assignment/dict/dict_q01.py
@@ -1,1 +1,7 @@
+#You have a dictionary containing employee IDs as keys and their names as values. Write a program to find the name of the employee with a specific ID.
+python
 
+employees = {101: 'Alice', 102: 'Bob', 103: 'Charlie'}
+employee_id = 102
+employee_name = employees.get(employee_id, 'Not Found')
+print(employee_name)
