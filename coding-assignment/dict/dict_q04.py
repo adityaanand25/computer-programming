6b1fc775d65a99fc935ccfec447bdc340c089e27
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:43:21 +0530

    Update dict_q04.py

--- coding-assignment/dict/dict_q04.py
+++ coding-assignment/dict/dict_q04.py
@@ -1,1 +1,11 @@
+#You have a list of items and want to categorize them into a dictionary based on their first letter.
+python
 
+items = ['apple', 'banana', 'apricot', 'blueberry']
+categorized = {}
+for item in items:
+    key = item[0]
+    if key not in categorized:
+        categorized[key] = []
+    categorized[key].append(item)
+print(categorized)
