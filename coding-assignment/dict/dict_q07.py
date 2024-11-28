07f06a55c5c4f1e26e58402b28b1fda99f091cab
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:45:29 +0530

    Update dict_q07.py

--- coding-assignment/dict/dict_q07.py
+++ coding-assignment/dict/dict_q07.py
@@ -1,1 +1,6 @@
+#Create a dictionary that maps country codes to country names and write a function to retrieve the country name by its code.
 
+country_codes = {'US': 'United States', 'CA': 'Canada', 'MX': 'Mexico'}
+def get_country_name(code):
+    return country_codes.get(code, 'Unknown Code')
+print(get_country_name('CA'))
