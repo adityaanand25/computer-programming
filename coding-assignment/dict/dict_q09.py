2c0065a7f5eb7aa7abc85b553d81990e82b0e92d
adityaanand25 <adityaanand250801@gmail.com>
2024-11-28 08:46:54 +0530

    Update dict_q09.py

--- coding-assignment/dict/dict_q09.py
+++ coding-assignment/dict/dict_q09.py
@@ -1,1 +1,7 @@
+#Write a program that counts the frequency of each character in a given string using a dictionary.
 
+text = "hello"
+char_count = {}
+for char in text:
+    char_count[char] = char_count.get(char, 0) + 1
+print(char_count)
