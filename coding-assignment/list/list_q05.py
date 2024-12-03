73c3759a23da8cb0113153ef393f58f6b15f213d
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 20:30:47 +0530

    Update list_q05.py

--- coding-assignment/list/list_q05.py
+++ coding-assignment/list/list_q05.py
@@ -1,1 +1,5 @@
-
+Partition a list into sublists of equal length
+def partition_list(lst, n):
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+lst = [1, 2, 3, 4, 5, 6, 7, 8]
+print(partition_list(lst, 3))
