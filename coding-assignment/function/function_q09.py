29a10cecaa4022eebed54cfea20ce8c016d57805
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:36:17 +0530

    Update function_q09.py

--- coding-assignment/function/function_q09.py
+++ coding-assignment/function/function_q09.py
@@ -1,1 +1,14 @@
-
+# Develop a simple calculator that can perform basic arithmetic operations.
+def calculator(a, b, operation):
+    if operation == 'add':
+        return a + b
+    elif operation == 'subtract':
+        return a - b
+    elif operation == 'multiply':
+        return a * b
+    elif operation == 'divide':
+        return a / b
+    else:
+        return "Invalid operation"
+print(calculator(10, 5, 'add'))
+print(calculator(10, 5, 'subtract'))
