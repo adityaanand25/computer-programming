8ad6acaee3acb219febc68382f8a99ae433ad56d
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:28:46 +0530

    Update function_q01.py

--- coding-assignment/function/function_q01.py
+++ coding-assignment/function/function_q01.py
@@ -1,1 +1,7 @@
+Solve a system of linear equations represented in matrix form
 
+import numpy as np
+A = np.array([[2, 3, 1], [1, 2, 3], [3, 1, 2]])
+b = np.array([7, 8, 9])
+x = np.linalg.solve(A, b)
+print(x)
