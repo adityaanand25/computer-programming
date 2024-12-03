bc483af97cf6e4b70bc3cb39d70158328fd14eee
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:36:40 +0530

    Update function_q10.py

--- coding-assignment/function/function_q10.py
+++ coding-assignment/function/function_q10.py
@@ -1,1 +1,10 @@
+Create a simple URL shortening service using Python dictionaries.
+python
+url_mapping = {}
 
+def shorten_url(original_url):
+    short_url = f"http://short.ly/{len(url_mapping) + 1}"
+    url_mapping[short_url] = original_url
+    return short_url
+
+print(shorten_url("http://example.com"))
