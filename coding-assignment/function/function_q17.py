16b742e0ff5e12541b138488b6f0b5d43159d135
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:44:50 +0530

    Update function_q17.py

--- coding-assignment/function/function_q17.py
+++ coding-assignment/function/function_q17.py
@@ -1,1 +1,12 @@
+Write a Python function that prints out the first n rows of Pascal's triangle.
 
+def pascal_triangle(n):
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        triangle.append(row)
+    return triangle
+for row in pascal_triangle(5):
+    print(row)
