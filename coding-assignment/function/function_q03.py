2c3ba74723b6cd6449afd38d984c4df5c591efc6
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:30:20 +0530

    Update function_q03.py

--- coding-assignment/function/function_q03.py
+++ coding-assignment/function/function_q03.py
@@ -1,1 +1,6 @@
-
+# Detect emotions in text data for sentiment analysis.
+from textblob import TextBlob
+def detect_emotion(text):
+    analysis = TextBlob(text)
+    return analysis.sentiment
+print(detect_emotion("I love programming!"))
