e4f07d1735efdfd940d14ffee6cb4fc867f97724
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:29:24 +0530

    Update function_q02.py

--- coding-assignment/function/function_q02.py
+++ coding-assignment/function/function_q02.py
@@ -1,1 +1,11 @@
+# Rename multiple files in a directory by adding a prefix.
 
+import os
+directory = '/path/to/files'
+prefix = 'new_'
+for filename in os.listdir(directory):
+    if not filename.startswith('.'):
+        filepath = os.path.join(directory, filename)
+        new_filename = prefix + filename
+        new_filepath = os.path.join(directory, new_filename)
+        os.rename(filepath, new_filepath)
