0fd9ec4f4c6e3ef38007033c4c659bb9289977b8
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 20:27:57 +0530

    Update list_q01.py

--- coding-assignment/list/list_q01.py
+++ coding-assignment/list/list_q01.py
@@ -1,1 +1,8 @@
+Write a program to rotate a list by k positions to the right
 
+Copy code
+def rotate_list(lst, k):
+    k = k % len(lst)
+    return lst[-k:] + lst[:-k]
+lst = [1, 2, 3, 4, 5, 6]
+print(rotate_list(lst, 2))
