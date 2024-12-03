f194148a4224b19087b4048c936a6fa977ad7ee3
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:40:10 +0530

    Update function_q14.py

--- coding-assignment/function/function_q14.py
+++ coding-assignment/function/function_q14.py
@@ -1,1 +1,7 @@
-
+Convert markdown text into HTML format.
+import markdown
+def convert_markdown_to_html(markdown_text):
+    return markdown.markdown(markdown_text)
+md_text = "# Hello World\nThis is markdown text."
+html_output = convert_markdown_to_html(md_text)
+print(html_output)
