7e28294f7c00154a5527e82a25db0c814b8a9c4c
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:38:43 +0530

    Update function_q12.py

--- coding-assignment/function/function_q12.py
+++ coding-assignment/function/function_q12.py
@@ -1,1 +1,8 @@
+Write a program to compress a text file using gzip.
 
+import gzip
+def compress_file(input_file, output_file):
+    with open(input_file, 'rb') as f_in:
+        with gzip.open(output_file, 'wb') as f_out:
+            f_out.writelines(f_in)
+compress_file('example.txt', 'example.txt.gz')
