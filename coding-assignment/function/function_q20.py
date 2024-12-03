2cc666125241397c058e4484d33b691befb40d61
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:47:04 +0530

    Update function_q20.py

--- coding-assignment/function/function_q20.py
+++ coding-assignment/function/function_q20.py
@@ -1,1 +1,6 @@
-
+ Write a Python program that invokes a function after a specified period of time.
+import time
+def delayed_function():
+    print("Function executed after delay!")
+time.sleep(2)
+delayed_function()
