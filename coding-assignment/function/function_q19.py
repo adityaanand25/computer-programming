f178c60289f2b127a89f886e5545dd09346800f4
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:46:36 +0530

    Update function_q19.py

--- coding-assignment/function/function_q19.py
+++ coding-assignment/function/function_q19.py
@@ -1,1 +1,8 @@
+ Write a Python program to detect the number of local variables declared in a function.
 
+def count_local_vars():
+    x = 10
+    y = "Hello"
+    z = [1, 2, 3]
+    return len(locals())
+print(count_local_vars())
