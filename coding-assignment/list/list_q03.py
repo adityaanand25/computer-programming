86f5e18c09f735201706cff12b63290206c78296
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 20:29:04 +0530

    Update list_q03.py

--- coding-assignment/list/list_q03.py
+++ coding-assignment/list/list_q03.py
@@ -1,1 +1,12 @@
+Flatten a deeply nested list
 
+def flatten_list(lst):
+    flat = []
+    for i in lst:
+        if isinstance(i, list):
+            flat.extend(flatten_list(i))
+        else:
+            flat.append(i)
+    return flat
+nested_list = [1, [2, [3, [4, 5]], 6], 7]
+print(flatten_list(nested_list))
