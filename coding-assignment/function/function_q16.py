c254a500558c9557faa8aa0fe26a14b1208a55cf
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:42:06 +0530

    Update function_q16.py

--- coding-assignment/function/function_q16.py
+++ coding-assignment/function/function_q16.py
@@ -1,1 +1,7 @@
+ Write a Python function that accepts a string and counts the number of upper and lower case letters.
 
+def count_case(s):
+    upper = sum(1 for c in s if c.isupper())
+    lower = sum(1 for c in s if c.islower())
+    return upper, lower
+print(count_case('The quick Brow Fox'))
