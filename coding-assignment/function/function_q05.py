ea919017091f62f9628298aae7c3fa566edea196
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:33:17 +0530

    Update function_q05.py

--- coding-assignment/function/function_q05.py
+++ coding-assignment/function/function_q05.py
@@ -1,1 +1,6 @@
+# Lalit is designing a game that requires simulating a dice roll. Help him implement this functionality.
 
+import random
+def roll_dice():
+    return random.randint(1, 6)
+print(f"You rolled a {roll_dice()}")
