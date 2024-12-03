02f0aa538671d6f78e2ce2aaab7fee34dc8c5902
adityaanand25 <adityaanand250801@gmail.com>
2024-12-03 10:45:23 +0530

    Update function_q18.py

--- coding-assignment/function/function_q18.py
+++ coding-assignment/function/function_q18.py
@@ -1,1 +1,7 @@
+Write a Python program to access a function inside another function.
 
+def outer_function():
+    def inner_function():
+        return "Hello from inner function!"
+    return inner_function()
+print(outer_function())
