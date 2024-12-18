56a6c349453adb79fd71979844ab5bdfdca725d6
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 10:08:00 +0530

    Update tuple_q01.py

--- coding-assignment/tuple/tuple_q01.py
+++ coding-assignment/tuple/tuple_q01.py
@@ -1,1 +1,15 @@
+ Flatten a Nested Tuple
 
+def flatten_tuple(nested_tup):
+    flat_list = []
+    def flatten(tup):
+        for item in tup:
+            if isinstance(item, tuple):
+                flatten(item)
+            else:
+                flat_list.append(item)
+    flatten(nested_tup)
+    return tuple(flat_list)
+
+result = flatten_tuple(((1, 2), (3, (4, 5))))
+print(result)  # Output: (1, 2, 3, 4, 5)
