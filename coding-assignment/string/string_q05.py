88d25d39154fc397718c2ff79023c10885c84293
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:50:43 +0530

    Update string_q05.py

--- coding-assignment/string/string_q05.py
+++ coding-assignment/string/string_q05.py
@@ -1,1 +1,4 @@
+Question: Remove duplicate characters from a string while maintaining order.
 
+def remove_duplicates(input_string):
+    return "".join(sorted(set(input_string), key=input_string.index))
