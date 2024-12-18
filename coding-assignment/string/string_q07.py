c31f36b719c2d286680a3c761168eebeec96f275
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:51:27 +0530

    Update string_q07.py

--- coding-assignment/string/string_q07.py
+++ coding-assignment/string/string_q07.py
@@ -1,1 +1,8 @@
+Question: Count occurrences of each word in a string.
 
+def count_word_occurrences(input_string):
+    words = input_string.split()
+    word_count = {}
+    for word in words:
+        word_count[word] = word_count.get(word, 0) + 1
+    return word_count
