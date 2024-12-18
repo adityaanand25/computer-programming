e0c3aae0e1fa35cafe9897fb5b4f772ab7597e9d
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:52:25 +0530

    Update string_q08.py

--- coding-assignment/string/string_q08.py
+++ coding-assignment/string/string_q08.py
@@ -1,1 +1,4 @@
+Question: Capitalize the first letter of each word in a string.
 
+def capitalize_first_letter(input_string):
+    return " ".join(word.capitalize() for word in input_string.split())
