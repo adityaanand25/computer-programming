b720618057342dab03ca41719f0ae375df7b46cc
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 10:10:48 +0530

    Update tuple_q05.py

--- coding-assignment/tuple/tuple_q05.py
+++ coding-assignment/tuple/tuple_q05.py
@@ -1,1 +1,14 @@
+def compare_tuples(tup1, tup2):
+    if tup1 < tup2:
+        return -1
+    elif tup1 > tup2:
+        return 1
+    else:
+        return 0
 
+result = compare_tuples((1,2), (1,3))
+print(result)  
+result = compare_tuples((1,), (1,))
+print(result)  
+result = compare_tuples((2,), (1,))
+print(result)  
