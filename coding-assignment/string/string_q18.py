9408345c93e0734376bdd8a0d279016c76ba82c7
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:56:40 +0530

    Update string_q18.py

--- coding-assignment/string/string_q18.py
+++ coding-assignment/string/string_q18.py
@@ -1,1 +1,13 @@
+Question: Find the longest common prefix among an array of strings.
 
+def longest_common_prefix(strings):
+    if not strings:
+        return ""
+    
+    prefix = strings[0]
+    
+    for string in strings[1:]:
+        while not string.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
