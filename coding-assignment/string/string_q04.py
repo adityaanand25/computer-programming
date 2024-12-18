82caa15ec5a729d9c9b827367a9d66fcfad1f463
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:50:19 +0530

    Update string_q04.py

--- coding-assignment/string/string_q04.py
+++ coding-assignment/string/string_q04.py
@@ -1,1 +1,7 @@
+Question: Find the most frequent character in a string.
 
+def most_frequent_char(input_string):
+    char_count = {}
+    for char in input_string:
+        char_count[char] = char_count.get(char, 0) + 1
+    return max(char_count, key=char_count.get)
