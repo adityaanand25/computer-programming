c4d4c058c78de789b2f77eed94f8882c2e260b16
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:53:46 +0530

    Update string_q11.py

--- coding-assignment/string/string_q11.py
+++ coding-assignment/string/string_q11.py
@@ -1,1 +1,4 @@
+Question: Replace all occurrences of a substring with another substring.
 
+def replace_substring(input_string, old_substring, new_substring):
+    return input_string.replace(old_substring, new_substring)
