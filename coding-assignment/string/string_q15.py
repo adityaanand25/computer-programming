f068e66ed0c56f74a2dc2c1aa1d7db71bab153d7
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:55:31 +0530

    Update string_q15.py

--- coding-assignment/string/string_q15.py
+++ coding-assignment/string/string_q15.py
@@ -1,1 +1,9 @@
+Question: Find the first non-repeated character in a string.
 
+from collections import Counter
+def first_non_repeated_char(input_string):
+    char_count = Counter(input_string)
+    for char in input_string:
+        if char_count[char] == 1:
+            return char
+    return None
