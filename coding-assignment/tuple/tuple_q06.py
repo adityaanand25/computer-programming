767c889ab00e7f00b258ea590243e392d7ce7ba1
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 10:11:39 +0530

    Update tuple_q06.py

--- coding-assignment/tuple/tuple_q06.py
+++ coding-assignment/tuple/tuple_q06.py
@@ -1,1 +1,10 @@
+ Find the Second Largest Element in a Tuple
+def second_largest(tup):
+    unique_elements = set(tup)
+    if len(unique_elements) < 2:
+        return None
+    unique_elements.remove(max(unique_elements))
+    return max(unique_elements)
 
+result = second_largest((1, 2, 3, 4, 5))
+print(result)
