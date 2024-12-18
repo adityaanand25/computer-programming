5e9e41c90572d9ba6505dc2d0c77f3634e2879ae
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 10:13:46 +0530

    Update tuple_q10.py

--- coding-assignment/tuple/tuple_q10.py
+++ coding-assignment/tuple/tuple_q10.py
@@ -1,1 +1,10 @@
+ Find the Frequency of Elements in a Tuple
 
+def frequency_of_elements(tup):
+    frequency = {}
+    for item in tup:
+        frequency[item] = frequency.get(item, 0) + 1
+    return frequency
+
+result = frequency_of_elements((1, 2, 2, 3, 3, 3))
+print(result)
