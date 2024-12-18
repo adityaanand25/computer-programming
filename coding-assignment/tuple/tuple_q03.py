a95a12d4f9dd4c04c0c6244aeb6d1b5a2613f88e
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 10:10:06 +0530

    Update tuple_q03.py

--- coding-assignment/tuple/tuple_q03.py
+++ coding-assignment/tuple/tuple_q03.py
@@ -1,1 +1,9 @@
+ Find Unique Elements Across Multiple Tuples
+def unique_elements(*tuples):
+    unique_set = set()
+    for tup in tuples:
+        unique_set.update(tup)
+    return tuple(unique_set)
 
+result = unique_elements((1, 2, 3), (3, 4), (4, 5))
+print(result)  # Output: (1, 2, 3, 4, 5)
