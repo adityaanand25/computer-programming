237e5eeca127cfcebbaddd47c50540fc5cb28333
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:57:44 +0530

    Update string_q19.py

--- coding-assignment/string/string_q19.py
+++ coding-assignment/string/string_q19.py
@@ -1,1 +1,6 @@
+Question: Move all spaces to the front of the string.
 
+def move_spaces_to_front(input_string):
+    spaces = ''.join(filter(lambda x: x == ' ', input_string))
+    non_spaces = ''.join(filter(lambda x: x != ' ', input_string))
+    return spaces + non_spaces
