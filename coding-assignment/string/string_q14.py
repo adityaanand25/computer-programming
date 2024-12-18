8ac5fb3f34ee9eea8cbc50e1a0dd379508fff0fa
adityaanand25 <adityaanand250801@gmail.com>
2024-12-18 09:55:00 +0530

    Update string_q14.py

--- coding-assignment/string/string_q14.py
+++ coding-assignment/string/string_q14.py
@@ -1,1 +1,6 @@
+Question: Check if an email address is valid using regex.
 
+import re
+def is_valid_email(email):
+    pattern = r'^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(pattern, email))
